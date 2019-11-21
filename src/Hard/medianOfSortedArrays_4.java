234619e83e59c0314c8cb7da927aab85aac73b5d
choumei1006 <1479784599@qq.com>
2019-11-22 00:21:03 +0800

    4-Median of two sorted arrays

--- src/Hard/medianOfSortedArrays_4.java
+++ src/Hard/medianOfSortedArrays_4.java
@@ -7,7 +7,7 @@ package Hard;
  * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
  * 你可以假设 nums1 和 nums2 不会同时为空。
  */
-public class medianOfSortedArrays {
+public class medianOfSortedArrays_4 {
     public static void main(String[] args) {
         int[] nums1 = {1,2};
         int[] nums2 = {3,4};
